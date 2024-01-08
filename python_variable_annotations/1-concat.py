705b1192d382409a0ad2cbdea4dae430e63007a8
Wayzer2318 <arezki.ait.kettout@gmail.com>
2024-01-08 17:33:19 +0100

    adding task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function that takes a str1 and
+     str2 and returns a concatenated string"""
+    return "{}{}".format(str1, str2)
