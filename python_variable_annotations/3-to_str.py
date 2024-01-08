eefb5d4d5c51cab074617f4f8677167e87f4971e
Wayzer2318 <arezki.ait.kettout@gmail.com>
2024-01-08 17:33:35 +0100

    adding task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""to string"""
+
+
+def to_str(n: float) -> str:
+    """ Write function that takes n
+    &as argument and returns the string of the float"""
+    return str(n)
